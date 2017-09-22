fc7462e1f644d923cfd1781bbd15ef07e60be14f
willlobato <will.lobato@gmail.com>
2017-09-21 22:17:54 -0300

    segundo commit

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-var inquirer = require('inquirer');
-
-const fs = require('fs');
-const extract = require('extract-zip');
+const inquirer = require('inquirer');
+const fs       = require('fs');
+const extract  = require('extract-zip');
+var CLI        = require('clui');
+var Spinner  = CLI.Spinner;
 
 const DIRECTORY = '/Users/willlobato/Projects/DEPLOY';
 const PREFIX_TEMP = '_';
+const ALL_PACKAGES = 'ALL PACKAGES';
+const TYPE_PACKAGE = {
+    EAR: 'ear',
+    WAR: 'war'
+}
 
 function extractFile(file, destination) {
     return new Promise(function(resolve, reject) {
@@ -15,32 +21,71 @@ function extractFile(file, destination) {
     });
 }
 
-async function openDeploy(directory, extension) {
+function listPackage(directory, extension) {
+    let files = fs.readdirSync(directory);
+    files = files.filter(file => file.indexOf('.' + extension) > 0);
+    return files;
+}
+
+function getFilesToProcess(directory, extension, parameter) {
+    let files = [];
+    if(parameter == ALL_PACKAGES) {
+        files = listPackage(directory, extension);
+    } else {
+        files = [parameter];
+    }
+    return files;
+}
+
+async function openPackage(directory, extension, status, parameter = ALL_PACKAGES) {
     try {
-        let files = fs.readdirSync(directory);
-        files = files.filter(file => file.indexOf('.' + extension) > 0);
+        let files = getFilesToProcess(directory, extension, parameter);
         for(let file of files) {
             let realPath = directory + '/' + file;
             if(!fs.lstatSync(realPath).isDirectory()) {
                 let realPathOpen = directory + '/' + PREFIX_TEMP + file;
                 if(!fs.existsSync(realPathOpen)) {
                     fs.mkdirSync(realPathOpen);
+                    status.message(`Extracting ${file} ...`);
                     var success = await extractFile(realPath, realPathOpen);
                     if(success) {
                         fs.unlinkSync(realPath);
                         fs.renameSync(realPathOpen, realPath);
-                        openDeploy(realPath, 'war');
+                        openPackage(realPath, TYPE_PACKAGE.WAR, status);
                     }
                 }    
             }
         }
+        status.stop();
     } catch(e) {
+        status.stop();
         console.error(e);
     }
 }
 
 async function main() {
-    openDeploy(DIRECTORY, 'ear');
+    //openDeploy(DIRECTORY, 'ear');
+    
+    let question = {
+        type: 'list',
+        name: 'file',
+        message: 'Which files do you want to open the packages?',
+        default: ALL_PACKAGES,
+        choices: [ALL_PACKAGES],
+
+    };
+
+    let packages = listPackage(DIRECTORY, 'ear');
+
+    question.choices = question.choices.concat(packages);
+
+    inquirer.prompt(question)
+    .then(function(answer) {
+        let status = new Spinner('Iniciando', ['⣾','⣽','⣻','⢿','⡿','⣟','⣯','⣷']);
+        status.start();
+        openPackage(DIRECTORY, 'ear', status, answer.file);
+    });
+
 }
 
 main();
